aa2c0f6d9d861a0e7cafc17d917bdf140c10b4bd
akasifislam <aseasifislam@gmail.com>
2021-05-23 13:06:27 +0600

    seed data

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 // post routing ** avoid resource routing**
-
-Route::get('/posts', 'PostController@index')->name('app.post.index');
-Route::get('/posts/create', 'PostController@create')->name('app.post.create');
-Route::post('/posts/store', 'PostController@store')->name('app.post.store');
-Route::get('/posts/{id}', 'PostController@show')->name('app.post.show');
-Route::get('/posts/{id}/edit', 'PostController@edit')->name('app.post.edit');
-Route::put('/posts/{id}/update', 'PostController@update')->name('app.post.update');
-Route::delete('/posts/{id}/destroy', 'PostController@destroy')->name('app.post.destroy');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/posts', 'PostController@index')->name('app.post.index');
+    Route::get('/posts/create', 'PostController@create')->name('app.post.create');
+    Route::post('/posts/store', 'PostController@store')->name('app.post.store');
+    Route::get('/posts/{id}', 'PostController@show')->name('app.post.show');
+    Route::get('/posts/{id}/edit', 'PostController@edit')->name('app.post.edit');
+    Route::put('/posts/{id}/update', 'PostController@update')->name('app.post.update');
+    Route::delete('/posts/{id}/destroy', 'PostController@destroy')->name('app.post.destroy');
+});
